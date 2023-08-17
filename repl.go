f7b897d2f6e2fe429cbd93bdc0d8a63ebec8913b
William Sánchez <swilliamd.sanchez@gmail.com>
2023-08-17 00:05:36 -0500

    Create two commands

--- repl.go
+++ repl.go
@@ -1,3 +1,70 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Printf("Please enter some command > ")
+
+		scanner.Scan()
+		text := scanner.Text()
+
+		cleanedInput := cleanInput(text)
+		if len(cleanedInput) == 0 {
+			continue
+		}
+
+		commandName := cleanedInput[0]
+		availableCommands := getCommands()
+
+		command, ok := availableCommands[commandName]
+		if !ok {
+			fmt.Println("invalid command!")
+			continue
+		}
+
+		err := command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
+}
+
+
+
+func cleanInput(input string) []string {
+	lowerCase := strings.ToLower(input)
+	words := strings.Fields(lowerCase)
+
+	return words
+}
+
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name: "help",
+			description: "Displays a help message.",
+			callback: commandHelp,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exit the program.",
+			callback: commandExit,
+		},
+	}
+}
